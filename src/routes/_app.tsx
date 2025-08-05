ebfe051c8297447140bb12371cd4a94437cb7f30
MisterChatlinks <166874035+MisterChatlinks@users.noreply.github.com>
2025-08-05 06:37:25 +0000

    feat: linked markdown css to app

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -15,6 +15,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Monitext</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/Styling/github-dark.css" />
         <link rel="icon" type="image/x-icon" href="/favicon.svg"></link>
       </head>
       <body>
