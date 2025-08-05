e3a15126c90e5013832aca1069ecc2d6689d4fc1
MisterChatlinks <166874035+MisterChatlinks@users.noreply.github.com>
2025-08-05 06:37:51 +0000

    feat: integrated footer component

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,5 +1,7 @@
 import HeaderIsland from "#islands/Components/Header/index.tsx";
 import { PageProps } from "$fresh/server.ts";
+import { Partial } from "$fresh/runtime.ts";
+import { FooterComponent } from "#components/statics/footer/index.tsx";
 
 export default function Layout({ Component }: PageProps) {
   return (
@@ -7,8 +9,13 @@ export default function Layout({ Component }: PageProps) {
       <div className="min-h-max">
         <HeaderIsland view="v3" />
       </div>
-      <div className="flex-auto p-8">
-        <Component/>
+      <div className="flex-auto p-8 overflow-auto">
+        <Partial name="main-content">
+          <Component />
+        </Partial>
+      </div>
+      <div>
+        <FooterComponent/>
       </div>
     </div>
   );
